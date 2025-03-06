671a4788ebc7b7ef3cb78fa15753cef95d0af48b
sganeshwani <ganeshwani2003@gmail.com>
2025-03-06 15:54:42 +0530

    [Sahil Ganeshwani] Add search and count functions for city/state

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,9 +1,13 @@
 class AddressBook {
-    addContact(contact) {
-        if (this.contacts.some(existing => existing.firstName === contact.firstName)) {
-            console.log("Duplicate Entry Found! Contact not added.");
-            return;
-        }
-        this.contacts.push(contact);
+    searchByCity(city) {
+        return this.contacts.filter(contact => contact.city === city);
+    }
+
+    countByCity(city) {
+        return this.searchByCity(city).length;
     }
 }
+
+// Example Usage
+console.log("Contacts in Bhopal:", addressBook.searchByCity("Bhopal"));
+console.log("Count of contacts in Bhopal:", addressBook.countByCity("Bhopal"));
